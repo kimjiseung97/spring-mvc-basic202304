1ac5833f4e8507d96d5a7bd367bd98a46de016e3
jisuengkim <dickseung23@naver.com>
2023-04-20 12:19:23 +0900

    # 4. 학생 성적 등록,조회,삭제 기능 구현

--- src/main/java/com/example/mvc/chap04/controller/ScoreController.java
+++ src/main/java/com/example/mvc/chap04/controller/ScoreController.java
@@ -1,9 +1,19 @@
 package com.example.mvc.chap04.controller;
 
+import com.example.mvc.chap04.dto.ScoreRequestDTO;
+import com.example.mvc.chap04.entity.Score;
+import com.example.mvc.chap04.repository.ScoreRepository;
+import com.example.mvc.chap04.repository.ScoreRepositoryImpl;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /*
 * # 요청 url
@@ -22,19 +32,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 * */
 @Controller
 @RequestMapping("/score")
+//@AllArgsConstructor : 모든 필드를 초기화하는 생성자
+@RequiredArgsConstructor // : final 필드만 초기화하는 생성자
 public class ScoreController {
+
+    //저장소에 의존해야 데이터를 받아서 클라이언트에 전달가능
+    private final ScoreRepository repository;
+    //만약에 클래스의 생성자가 단 한1개라면
+    //자동으로 @Autowired를 써줌
+
+//   @Autowired
+//    public ScoreController(ScoreRepository repository){
+//        this.repository = repository;
+//    }
     //성적 등록화면 띄우기 + 정보목록 조회
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
         System.out.println("/score/list : GET!");
-        return "";
+        List<Score> allScore = repository.findAll();
+        model.addAttribute("sList",allScore);
+        return "chap04/score-list";
     }
 
     //성적정보 등록 처리 요청
     @PostMapping("/register")
-    public String register(){
-        System.out.println("/score/register : POST!");
-        return "";
+    public String register(ScoreRequestDTO dto){
+
+        //입력데이터(쿼리스트링) 읽기
+        System.out.println("/score/register : POST! -"+dto);
+
+        //dto(Score)를 entity(Score)로 변환해야함
+        Score score = new Score(dto);
+        //save명령
+        repository.saveScore(score);
+        /*
+        * 등록요청에서 jsp뷰 포워딩을하면
+        * 갱신된 목록을 다시한번 저장소에서 불러와
+        * 모델에 담는 추가적인 코드가 필요하지만
+        *
+        *
+        * 리다이렉트를 통해서 위에서 만든 /score/list : GET
+        * 을 자동으로 다시 보낼 수  있다면 번거러운 코드가
+        * 줄어들 수 있겠다.*/
+        return "redirect:/score/list";
     }
 
     //성적 정보 삭제 요청
--- src/main/java/com/example/mvc/chap04/repository/ScoreRepositoryImpl.java
+++ src/main/java/com/example/mvc/chap04/repository/ScoreRepositoryImpl.java
@@ -1,27 +1,64 @@
 package com.example.mvc.chap04.repository;
 
+import com.example.mvc.chap04.entity.Grade;
 import com.example.mvc.chap04.entity.Score;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static com.example.mvc.chap04.entity.Grade.*;
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
+
+@Repository //스프링 빈 등록 : 객체생성의 제어권을 스프링에게 위임
 public class ScoreRepositoryImpl  implements ScoreRepository{
-    @Override
+
+    //key : 학번, value : 성적정보
+    private final static Map<Integer, Score> scoreMap;
+
+    //학번에 사용할 일련번호
+    private static int sequence;
+
+    static {
+        scoreMap = new HashMap<>();
+        Score stu1 = new Score("뽀로로", 100, 50, 70, ++sequence, 0,0, A);
+        Score stu2 = new Score("춘식이", 33, 56, 12, ++sequence, 0,0,  B);
+        Score stu3 = new Score("대길이", 88, 12, 50, ++sequence, 0,0, C);
+        scoreMap.put(stu1.getStuNum(),stu1);
+        scoreMap.put(stu2.getStuNum(),stu2);
+        scoreMap.put(stu3.getStuNum(),stu3);
+    }
+
     public List<Score> findAll() {
-        return null;
+        return new ArrayList<>(scoreMap.values())
+                .stream()
+                .sorted(comparing(score -> score.getStuNum()))
+                .collect(toList())
+                ;
     }
 
     @Override
     public boolean saveScore(Score score) {
-        return false;
+        if(scoreMap.containsKey(score.getStuNum())){
+            return false;
+        }
+        score.setStuNum(++sequence);
+        scoreMap.put(score.getStuNum(),score);
+        System.out.println(findAll());
+        return true;
     }
 
     @Override
     public boolean deleteScoreByStuNum(int stuNum) {
-        return false;
+        if(!scoreMap.containsKey(stuNum)) return false;
+        scoreMap.remove(stuNum);
+        return true;
     }
 
     @Override
     public Score findScoreByStuNum(int stuNum) {
-        return null;
+        return scoreMap.get(stuNum);
     }
 }
